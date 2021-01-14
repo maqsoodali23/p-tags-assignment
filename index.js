a3b2caddb88c7400387e44f86661630d4c17a0ea
Maqsood Shah <maqsood.ali@invozone.com>
2021-01-14 17:02:26 +0500

    Applying prettier and eslint

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello to the future');
+    res.send('Hello to the future');});
+app.get('/about', (req, res) => {
+    res.send('Welcome to about us page');
 });
 
 app.listen(port, () => {
