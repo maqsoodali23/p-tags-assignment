e2fad53329cae64f33ccbd13c3d6cf0aea4f5299
Maqsood Shah <maqsood.ali@invozone.com>
2021-01-14 17:53:16 +0500

    Applying airbnb rules with prettier

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from 'express';
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello to the future');});
+  res.send('Hello to the future');
+});
 app.get('/about', (req, res) => {
-    res.send('Welcome to about us page');
+  res.send('Welcome to about us page');
 });
 
 app.listen(port, () => {
-    console.log(`App is listning to port ${port}`);
+  //   console.log(`App is listning to port ${port}`);
 });
