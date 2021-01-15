349782cad239ef038f7c5b1000883f7700725dc1
Maqsood Shah <maqsood.ali@invozone.com>
2021-01-15 13:27:46 +0500

    Updating tag removal code

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import express from 'express';
+import { homedir } from 'os';
+import { writeFile, readFile, readFileSync } from 'fs';
 
 const app = express();
 const port = 3000;
 
+const homedirectory = homedir();
+const sourceFile = `${homedirectory}/Documents/12.html`;
+const targetFile = `${homedirectory}/Documents/target.html`;
+readFile(sourceFile, 'utf8', (err, data) => {
+  if (err) throw err;
+  const stripedHtml = data.replace(/<[p/>][p>]*><\/[p>]+>/g, '');
+  writeFile(targetFile, stripedHtml, (err) => {});
+  if (err) throw err;
+  console.log('The updated file has been saveed successfully');
+}); 
+
 app.get('/', (req, res) => {
-  res.send('Hello to the future');
-});
-app.get('/about', (req, res) => {
-  res.send('Welcome to about us page');
+  res.send('Welcome to the landing page');
 });
 
 app.listen(port, () => {
-  //   console.log(`App is listning to port ${port}`);
+  console.log(`App is listening to port ${port}`);
 });
